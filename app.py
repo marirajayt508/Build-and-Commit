fc15ea980542e72806f9e4db855ce54184b25973
Mariraja Selvaraja <marirajayt508@gmail.com>
2022-01-23 09:43:41 +0000

    'Commited'

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask,render_template
+from os import system
 app = Flask(__name__)
 
 @app.route('/')
 def index():
    return render_template("index.html")
+
+@app.route('/build')
+def build():
+   system("cd..'")
+   system("git add *")
+   system("dir")
+   system("git commit -m 'Commited'")
+   return "Build Started"
 
 if __name__ == '__main__':
    app.run(debug = True)
